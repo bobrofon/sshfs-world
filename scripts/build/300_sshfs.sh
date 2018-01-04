9710a60b3d43d8036ba9abf0b499c4c18e46f5a0
bobrofon <bobrofon@gmail.com>
2018-01-04 16:49:23 +0700

    sshfs with fuse3

--- scripts/build/300_sshfs.sh
+++ scripts/build/300_sshfs.sh
@@ -2,19 +2,22 @@
 set -eu
 
 ROOT=$PWD
+PREFIX=$ROOT/prefix
 
 mkdir -p build/sshfs
 mkdir -p prefix
-mkdir -p trash
 cd build/sshfs
 
-PKG_CONFIG_PATH=$ROOT/prefix/lib/pkgconfig \
-$ROOT/src/sshfs/configure \
-	CC=gcc-static \
-	SSHFS_LIBS="-L/src/prefix/lib -lfuse -lgthread-2.0 -pthread -lglib-2.0 -ldl" \
-	--prefix=$ROOT/trash \
-	--bindir=$ROOT/prefix/bin \
-	--host=$HOST
+PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/lib64/pkgconfig" \
+CC=gcc-static \
+meson \
+	--prefix=$PREFIX \
+	--backend ninja \
+	--buildtype release \
+	--strip \
+	--unity off \
+	--default-library static \
+	$ROOT/src/sshfs
 
-make
-make install
+ninja
+ninja install
